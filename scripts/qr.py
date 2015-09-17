ab6b88dbf3dd7ca4d6b602fe542293afac9bec4f
sasha42 <sasha42@users.noreply.github.com>
2015-09-17 06:17:16 +0200

    Moved all key features to qr.py

--- scripts/qr.py
+++ scripts/qr.py
@@ -3,11 +3,20 @@
 import qrcode
 from PIL import Image
 import sys
-import pdb
 import subprocess
+import datetime
 
-qr_data = sys.stdin.read()
+# Set variables
+string = sys.stdin.read()
+qr_data = string.split("|")[4]
+time = datetime.datetime.now().strftime("%Y-%m-%d|%H:%M:%S|")
+
+# Create and print QR
 qrcode_image = qrcode.make(qr_data, border=0)
 qrcode_image = qrcode_image.resize((720, 720), Image.NEAREST)
 qrcode_image.save("/scripts/qrcode.png")
 subprocess.call(["/scripts/ql570/ql570", "/dev/usb/lp0", "w", "/scripts/qrcode.png"])
+
+# Log what happened
+with open("/var/www/printlog", "a") as logfile:
+    logfile.write(time + string)
