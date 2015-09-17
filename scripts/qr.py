076dced28c341ad68274ef616d3cb683dae90563
sasha42 <sasha42@users.noreply.github.com>
2015-09-17 05:20:54 +0200

    Moved printing into qr.py using subprocess

--- scripts/qr.py
+++ scripts/qr.py
@@ -4,8 +4,10 @@ import qrcode
 from PIL import Image
 import sys
 import pdb
+import subprocess
 
 qr_data = sys.stdin.read()
 qrcode_image = qrcode.make(qr_data, border=0)
 qrcode_image = qrcode_image.resize((720, 720), Image.NEAREST)
-qrcode_image.save("qrcode.png")
+qrcode_image.save("/scripts/qrcode.png")
+subprocess.call(["/scripts/ql570/ql570", "/dev/usb/lp0", "w", "/scripts/qrcode.png"])
--- scripts/qr.sh
+++ scripts/qr.sh
@@ -3,7 +3,7 @@ TEXT=$(echo $* | cut -d "|" -f5-)
 #qrencode -o qr.png "$TEXT"
 echo "$TEXT" | python /scripts/qr.py
 #lp -d Brother_QL-700 qrcode.png
-/scripts/ql570/ql570 /dev/usb/lp0 w qrcode.png
+#/scripts/ql570/ql570 /dev/usb/lp0 w qrcode.png
 screen -dm mpg123 /scripts/yt/printed.mp3
 #rm qrcode.png
 DATE=$(date "+%Y/%m/%d|%H:%M:%S")
